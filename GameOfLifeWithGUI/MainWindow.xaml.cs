1c591bf429652663576456238671073d42f6cacb
Patrik Gustafsson <patrikg123@hotmail.com>
2020-05-29 15:42:31 +0200

    Creating te board with cells

--- GameOfLifeWithGUI/MainWindow.xaml.cs
+++ GameOfLifeWithGUI/MainWindow.xaml.cs
@@ -27,7 +27,22 @@ namespace GameOfLifeWithGUI
 
         private void ButtonStart(object sender, RoutedEventArgs e)
         {
+            int antalCellerLangden = 30;
+            int antalCellerHojden = 30;
 
+            for (int i = 0; i < antalCellerHojden; i++)
+            {
+                for (int j = 0; j < antalCellerLangden; j++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Width = Brade.ActualWidth / antalCellerLangden - 3.0;
+                    r.Height = Brade.ActualHeight / antalCellerHojden -3.0;
+                    r.Fill = Brushes.Red;
+                    Brade.Children.Add(r);
+                    Canvas.SetLeft(r, j * Brade.ActualWidth / antalCellerLangden);
+                    Canvas.SetTop(r, i * Brade.ActualHeight / antalCellerHojden);
+                }
+            }
         }
     }
 }
